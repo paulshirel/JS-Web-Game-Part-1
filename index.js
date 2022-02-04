7cf94ceecf07e3a0db37f4be3f1cbe5fc68c3049
Paul Shirer <paulshirer@gmail.com>
2022-02-04 17:15:18 -0600

    added pineTree

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
-alert('yes')
-alert('no')   
-////git commit -m "changed font size on navbar"
-//
+
+
+let pineTree = document.createElement('img')
+greenCharacter.src = 'assets/pine-tree.png'
+greenCharacter.style.position = 'fixed'
+greenCharacter.style.left = '450px'
+greenCharacter.style.bottom = '200px'
+document.body.append(pineTree)
